72bee1dbfa8871dd4887cd6494cec0b1f5f5c60e
RaphoufouLeFou <121761197+RaphoufouLeFou@users.noreply.github.com>
2024-01-16 17:06:22 +0100

    added Load Game button
    !noping

--- Assets/Scripts/MainMenu/MainMenu.cs
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -2,70 +2,66 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-/*
-public static class NetworkInfos
-{
-    public static bool IsMultiplayerGame { get; set; }
-}
-*/
+using UnityEngine.Serialization;
+
 public class MainMenu : MonoBehaviour
 {
 
-    [SerializeField] private string MainSceneName = "Main";
-    [SerializeField] private GameObject MainParent;
-    [SerializeField] private GameObject NewGameParent;
-    [SerializeField] private GameObject LoadGameParent;
-    [SerializeField] private GameObject BackButton;
+    [SerializeField] private string mainSceneName = "Main";
+    [SerializeField] private GameObject mainParent;
+    [SerializeField] private GameObject newGameParent;
+    [SerializeField] private GameObject loadGameParent;
+    [SerializeField] private GameObject backButton;
 
     void Start()
     {
-        BackButton.SetActive(false);
-        MainParent.SetActive(true);
-        NewGameParent.SetActive(false);
-        LoadGameParent.SetActive(false);
+        backButton.SetActive(false);
+        mainParent.SetActive(true);
+        newGameParent.SetActive(false);
+        loadGameParent.SetActive(false);
     }
     void Update() { }
 
     public void NewGameButtonClick()
     {
-        BackButton.SetActive(true);
-        MainParent.SetActive(false);
-        NewGameParent.SetActive(true);
+        backButton.SetActive(true);
+        mainParent.SetActive(false);
+        newGameParent.SetActive(true);
     }
 
     public void BackButtonClick()
     {
-        MainParent.SetActive(true);
-        NewGameParent.SetActive(false);
-        LoadGameParent.SetActive(false);
-        BackButton.SetActive(false);
+        mainParent.SetActive(true);
+        newGameParent.SetActive(false);
+        loadGameParent.SetActive(false);
+        backButton.SetActive(false);
     }
 
     public void LoadGameButtonClick()
     {
-        BackButton.SetActive(true);
-        MainParent.SetActive(false);
-        LoadGameParent.SetActive(true);
+        backButton.SetActive(true);
+        mainParent.SetActive(false);
+        loadGameParent.SetActive(true);
     }
     public void JoinGameButtonClick()
     {
         NetworkInfos.IsMultiplayerGame = true;
         NetworkInfos.IsHost = false;
         NetworkInfos.StartedFromMainMenu = true;
-        SceneManager.LoadScene(MainSceneName);
+        SceneManager.LoadScene(mainSceneName);
     }
     public void MultiPlayerButtonClick()
     {
         NetworkInfos.IsMultiplayerGame = true;
         NetworkInfos.IsHost = true;
         NetworkInfos.StartedFromMainMenu = true;
-        SceneManager.LoadScene(MainSceneName);
+        SceneManager.LoadScene(mainSceneName);
     }
     public void SinglePlayerButtonClick()
     {
         NetworkInfos.IsMultiplayerGame = false;
         NetworkInfos.IsHost = true;
         NetworkInfos.StartedFromMainMenu = true;
-        SceneManager.LoadScene(MainSceneName);
+        SceneManager.LoadScene(mainSceneName);
     }
 }
