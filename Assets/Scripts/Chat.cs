46b1f9de20ac98901d0805ccaf90b670cf53aa10
RaphoufouLeFou <121761197+RaphoufouLeFou@users.noreply.github.com>
2024-01-18 01:48:39 +0100

    Added chat

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -1,48 +1,77 @@
-using System;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
 
 public class Chat : NetworkBehaviour
 {
     // Start is called before the first frame update
-    private List<GameObject> _messages;
-    public GameObject ContentParent;
-    public GameObject MessagePrefab;
-    public GameObject ChatWindow;
+    [SerializeField] private List<GameObject> messages = new ();
+    public GameObject contentParent;
+    public GameObject messagePrefab;
+    public GameObject chatWindow;
 
     private void Start()
     {
-        ChatWindow.SetActive(false);
+        if (!isServer)
+        {
+            SendMessages("A new player joined !");
+        }
+        chatWindow.SetActive(true);
     }
 
     [ClientRpc]
+    private void RcpClientReceveMessageFromUser(string prefix ,string message)
+    {
+        GameObject msg = Instantiate(messagePrefab, Vector3.zero, Quaternion.identity, contentParent.transform);
+        msg.GetComponent<TMP_Text>().text = $"{prefix} : {message}";
+        messages.Add(msg);
+    }
+    
+    [Command(requiresAuthority = false)]
+    private void CommandReceveMessageFromUser(string prefix, string message)
+    {
+        RcpClientReceveMessageFromUser(prefix, message);
+    }
+
+    public void SendMessagesFromUser(string message)
+    {
+        if (isServer) RcpClientReceveMessageFromUser("Server", message);
+        else CommandReceveMessageFromUser("Client", message);
+    }
+
+    
+    [ClientRpc]
     private void RcpClientReceveMessage(string message)
     {
-        GameObject msg = Instantiate(MessagePrefab, Vector3.zero, Quaternion.identity, ContentParent.transform);
-        msg.GetComponent<TMP_Text>().text = $"Server : {message}";
-        _messages.Add(msg);
+        GameObject msg = Instantiate(messagePrefab, Vector3.zero, Quaternion.identity, contentParent.transform);
+        msg.GetComponent<TMP_Text>().text = $"{message}";
+        messages.Add(msg);
     }
     
-    [Command]
-    private void CommandReceveMessage(string message)
+    [Command(requiresAuthority = false)]
+    private void CommandReceveMessage( string message)
     {
-        GameObject msg = Instantiate(MessagePrefab, Vector3.zero, Quaternion.identity, ContentParent.transform);
-        msg.GetComponent<TMP_Text>().text = $"Client : {message}";
-        _messages.Add(msg);
+        RcpClientReceveMessage(message);
     }
-    
-    public void SendMessage(string message)
+    public void SendMessages(string message)
     {
         if (isServer) RcpClientReceveMessage(message);
         else CommandReceveMessage(message);
     }
-
+    
     public void SendMessageFromChatUI(GameObject self)
     {
         string msg = self.GetComponent<TMP_InputField>().text;
-        SendMessage(msg);
+        SendMessagesFromUser(msg);
+    }
+
+    public void ClearHistory()
+    {
+        foreach (GameObject msg in messages)
+        {
+            Destroy(msg);
+        }
+        messages.Clear();
     }
 }
