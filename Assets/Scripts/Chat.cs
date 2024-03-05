f2a594b2ff6c4fd3b7a4c8b384dcb602b17fe2b1
RaphoufouLeFou <121761197+RaphoufouLeFou@users.noreply.github.com>
2024-03-06 00:47:29 +0100

    improved chat

--- Assets/Scripts/Chat.cs
+++ Assets/Scripts/Chat.cs
@@ -1,12 +1,28 @@
 using System.Collections.Generic;
+using System.Linq;
 using Mirror;
 using UnityEngine;
 using TMPro;
 
+public class Msg
+{
+    public GameObject Obj;
+    private float _endTime;
+
+    public Msg(GameObject obj, float lifeTime)
+    {
+        Obj = obj;
+        _endTime = Time.time + lifeTime;
+    }
+    public bool IsTimesUp() => Time.time >= _endTime;
+    
+}
 public class Chat : NetworkBehaviour
 {
     // Start is called before the first frame update
-    private Queue<GameObject> _messages = new ();
+    private Queue<Msg> _messages = new();
+    private List<string> _pastMessages = new();
+    private int _pastIndex;
     
     public GameObject contentParent;
     public GameObject messagePrefab;
@@ -25,8 +41,8 @@ public class Chat : NetworkBehaviour
     private void RcpClientReceiveMessageFromUser(string prefix ,string message)
     {
         GameObject msg = Instantiate(messagePrefab, Vector3.zero, Quaternion.identity, contentParent.transform);
-        msg.GetComponent<TMP_Text>().text = $"{prefix} : {message}";
-        _messages.Enqueue(msg);
+        msg.GetComponentInChildren<TMP_Text>().text = $"{prefix} : {message}";
+        _messages.Enqueue(new Msg(msg, 5));
     }
     
     [Command(requiresAuthority = false)]
@@ -45,9 +61,9 @@ public class Chat : NetworkBehaviour
     private void RcpClientReceiveMessage(string message)
     {
         GameObject msg = Instantiate(messagePrefab, Vector3.zero, Quaternion.identity, contentParent.transform);
-        msg.GetComponent<TMP_Text>().text = $"{message}";
-        msg.GetComponent<TMP_Text>().color = Color.yellow;
-        _messages.Enqueue(msg);
+        msg.GetComponentInChildren<TMP_Text>().text = $"{message}";
+        msg.GetComponentInChildren<TMP_Text>().color = Color.yellow;
+        _messages.Enqueue(new Msg(msg, 5));
     }
     
     [Command(requiresAuthority = false)]
@@ -63,7 +79,15 @@ public class Chat : NetworkBehaviour
     
     public void SendMessageFromChatUI(GameObject self)
     {
-        string msg = self.GetComponent<TMP_InputField>().text;
+        TMP_InputField inputField = self.GetComponent<TMP_InputField>();
+        string msg = inputField.text;
+        if(msg.Length == 0) return;
+        if (msg.Length == msg.Count(f => f == ' ')) return;
+        inputField.text = "";
+        _pastIndex = -1;
+        _pastMessages.Add(msg);
+        inputField.Select();
+        inputField.ActivateInputField();
         SendMessagesFromUser(msg);
     }
     
@@ -75,12 +99,38 @@ public class Chat : NetworkBehaviour
         if (Input.GetKeyDown(Settings.KeyMap["Chat"]) && Settings.Playing)
         {
             chatWindow.SetActive(true);
+            chatWindow.GetComponentInChildren<TMP_InputField>().Select();
+            chatWindow.GetComponentInChildren<TMP_InputField>().ActivateInputField();
+            Settings.IsPaused = true;
             Settings.Playing = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Return) && chatWindow.activeSelf)
-            SendMessageFromChatUI(transform.GetChild(0).gameObject);
-        
-        while (_messages.Count > 10) Destroy(_messages.Dequeue());
+        if ((Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter)) && chatWindow.activeSelf)
+            SendMessageFromChatUI(chatWindow.transform.GetChild(0).gameObject);
+
+        foreach (Msg o in _messages)
+        {
+            if(o.IsTimesUp()) o.Obj.SetActive(chatWindow.activeSelf);
+        }
+        while (_messages.Count > 10) Destroy(_messages.Dequeue().Obj);
+
+        if (chatWindow.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && _pastIndex < _pastMessages.Count)
+            {
+                _pastIndex++;
+                if (_pastIndex == _pastMessages.Count)_pastIndex --;
+                if(_pastMessages.Count != 0)
+                    chatWindow.GetComponentInChildren<TMP_InputField>().text = _pastMessages[_pastMessages.Count - 1 - _pastIndex];
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow) && _pastIndex > 0)
+            {
+                chatWindow.GetComponentInChildren<TMP_InputField>().text = _pastMessages[_pastMessages.Count - 1 - _pastIndex];
+                if (_pastIndex - 1 >= 0 && _pastIndex - 1 < _pastMessages.Count) _pastIndex--;
+            }
+        }
     }
+    
+    
 }
